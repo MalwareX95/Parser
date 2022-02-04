73e395075cd09386d3ffd96bbbd05aaac751958c
Kamil <graphandfun@hotmail.com>
2022-02-04 17:40:14 +0100

    add support for comments

--- tests/run.ts
+++ tests/run.ts
@@ -2,8 +2,16 @@ import { Parser } from "../src/Parser";
 
 const parser = new Parser();
 
-const program = `"hello"`;
+const program = `
+    /*
+     * Documentation comment
+     */
+    42;
+`;
 
+/**
+ * 
+ */
 const ast = parser.parse(program);
 
 console.log(JSON.stringify(ast, null, 2));
